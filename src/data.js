8617de2302cb0e78e3a36c4e93fec2f4ed9d93d8
muhammadcod <adebayomuhammad47@gmail.com>
2023-07-10 22:37:59 +0100

    first commit

---
+++ src/data.js
@@ -0,0 +1,262 @@
+[
+  {
+    "title": "deleniti sequi voluptate",
+    "description": "Quo id repellendus. Assumenda libero nostrum minima saepe laborum. Neque consequuntur deleniti possimus.",
+    "price": "69.00",
+    "name": "Trevor Kulas",
+    "availableDates": "2023-07-12T12:01:42.044Z",
+    "location": {
+      "city": "Reneefort",
+      "country": "Lake Havasu City"
+    },
+    "url": require("../assets/images/free-stock-image-1.jpg"),
+    "rating": 5
+  },
+  {
+    "title": "voluptatibus numquam molestias",
+    "description": "Libero corrupti necessitatibus laborum nostrum labore quidem beatae sint. Dicta dolore autem quia ipsa nostrum magni excepturi dicta minus. Quod harum consequatur ab esse dolor.",
+    "price": "307.00",
+    "name": "Bob Feeney",
+    "availableDates": "2023-11-14T08:54:40.471Z",
+    "location": {
+      "city": "Ceres",
+      "country": "Bountiful"
+    },
+    "url": require("../assets/images/free-stock-image-1.jpg"),
+    "rating": 1
+  },
+  {
+    "title": "nihil harum nobis",
+    "description": "Illum iste nemo rem optio natus beatae minus. Sit nemo fuga voluptas recusandae ipsam sint ducimus. Nisi inventore debitis eaque odit quas eaque quos.",
+    "price": "93.00",
+    "name": "Nina Okuneva",
+    "availableDates": "2023-09-14T04:58:20.584Z",
+    "location": {
+      "city": "Enidborough",
+      "country": "Reinaborough"
+    },
+    "url": require("../assets/images/free-stock-image-1.jpg"),
+    "rating": 1
+  },
+  {
+    "title": "itaque itaque fugit",
+    "description": "Nulla ipsum nemo. Distinctio libero nam ipsum laborum quasi dolore ea optio. Odit cumque quod nostrum perferendis.",
+    "price": "461.00",
+    "name": "Monica McLaughlin",
+    "availableDates": "2023-07-28T20:34:31.256Z",
+    "location": {
+      "city": "Kertzmannstad",
+      "country": "Fort Lauri"
+    },
+    "url": require("../assets/images/free-stock-image-1.jpg"),
+    "rating": 4
+  },
+  {
+    "title": "nulla et deserunt",
+    "description": "Reprehenderit quae omnis maiores iusto officia ab tempore incidunt expedita. Itaque labore laboriosam non inventore molestias reprehenderit perferendis. Possimus dolore tenetur aspernatur fuga laudantium eligendi.",
+    "price": "194.00",
+    "name": "Mr. Sean MacGyver Sr.",
+    "availableDates": "2023-08-16T17:48:18.034Z",
+    "location": {
+      "city": "Cedar Hill",
+      "country": "Dejahcester"
+    },
+    "url": require("../assets/images/free-stock-image-1.jpg"),
+    "rating": 2
+  },
+  {
+    "title": "vero debitis ad",
+    "description": "Debitis excepturi deserunt accusantium accusantium sequi asperiores occaecati laudantium deserunt. Cum eum error reprehenderit optio. Omnis fuga quae saepe ducimus culpa aut vitae et.",
+    "price": "168.00",
+    "name": "Jessie Shanahan",
+    "availableDates": "2023-10-14T09:26:19.960Z",
+    "location": {
+      "city": "New Isaiah",
+      "country": "New Humbertoport"
+    },
+    "url": require("../assets/images/free-stock-image-1.jpg"),
+    "rating": 3
+  },
+  {
+    "title": "ratione fugiat distinctio",
+    "description": "Cumque possimus quaerat voluptate id. Nam provident maxime tempore. Vero soluta nisi dolorum possimus odit veniam at vel.",
+    "price": "239.00",
+    "name": "Laurie Price",
+    "availableDates": "2023-07-02T20:05:15.657Z",
+    "location": {
+      "city": "Lake Ariport",
+      "country": "Vilmatown"
+    },
+    "url": require("../assets/images/free-stock-image-1.jpg"),
+    "rating": 1
+  },
+  {
+    "title": "aliquam enim unde",
+    "description": "Fugit veniam corporis nobis. Voluptatem eius modi ipsum est eligendi est id debitis. Hic alias ipsam quo facilis voluptatum.",
+    "price": "462.00",
+    "name": "Dr. Peter Sporer-Gutkowski V",
+    "availableDates": "2023-09-20T11:32:19.584Z",
+    "location": {
+      "city": "Tatumland",
+      "country": "Powlskiborough"
+    },
+    "url": require("../assets/images/free-stock-image-1.jpg"),
+    "rating": 5
+  },
+  {
+    "title": "eaque earum quas",
+    "description": "Voluptates consequatur blanditiis dolore ratione. Ex labore commodi porro aliquid. Consequuntur dolores fuga nihil voluptas.",
+    "price": "321.00",
+    "name": "Willis Bins",
+    "availableDates": "2023-07-13T15:12:29.871Z",
+    "location": {
+      "city": "New Brando",
+      "country": "Ashaview"
+    },
+    "url": require("../assets/images/free-stock-image-1.jpg"),
+    "rating": 5
+  },
+  {
+    "title": "repellat expedita aut",
+    "description": "Hic iste libero autem. Doloribus aut debitis odio. Dignissimos est non vero sint nemo sint aliquid est.",
+    "price": "351.00",
+    "name": "Armando Ryan",
+    "availableDates": "2023-08-07T07:16:47.582Z",
+    "location": {
+      "city": "Visalia",
+      "country": "New Tyra"
+    },
+    "url": require("../assets/images/free-stock-image-1.jpg"),
+    "rating": 4
+  },
+  {
+    "title": "et placeat aliquid",
+    "description": "Explicabo aliquid delectus molestias ipsam. Autem laborum sint ut a explicabo impedit ea veniam corrupti. Sequi tempore reiciendis accusamus porro illum sapiente sequi id.",
+    "price": "490.00",
+    "name": "Cary Breitenberg",
+    "availableDates": "2023-08-16T03:02:24.405Z",
+    "location": {
+      "city": "West Rowlandfurt",
+      "country": "Flagstaff"
+    },
+    "url": require("../assets/images/free-stock-image-1.jpg"),
+    "rating": 3
+  },
+  {
+    "title": "perspiciatis dolor praesentium",
+    "description": "Quae esse nisi qui. Fuga earum quo tempora dolor fugiat est ad quaerat cum. Nulla numquam asperiores distinctio fuga.",
+    "price": "208.00",
+    "name": "Dr. Meredith Steuber",
+    "availableDates": "2023-09-29T04:22:36.811Z",
+    "location": {
+      "city": "Port Gennaroboro",
+      "country": "Riverton"
+    },
+    "url": require("../assets/images/free-stock-image-1.jpg"),
+    "rating": 1
+  },
+  {
+    "title": "deleniti illo repellat",
+    "description": "Quasi vitae saepe. Recusandae praesentium quidem repellat nostrum debitis mollitia nihil. Sed ab accusamus sed aspernatur.",
+    "price": "169.00",
+    "name": "Kirk Tremblay Sr.",
+    "availableDates": "2023-08-10T00:43:12.239Z",
+    "location": {
+      "city": "Sheldonstead",
+      "country": "New Frankiefort"
+    },
+    "url": require("../assets/images/free-stock-image-1.jpg"),
+    "rating": 1
+  },
+  {
+    "title": "nesciunt eaque est",
+    "description": "Voluptate exercitationem officia. Temporibus voluptatibus aperiam inventore. Corrupti dolore rem ipsam nobis laboriosam libero ut tenetur excepturi.",
+    "price": "390.00",
+    "name": "Terence Conn",
+    "availableDates": "2023-07-27T01:59:03.496Z",
+    "location": {
+      "city": "Heidenreichboro",
+      "country": "Eastvale"
+    },
+    "url": require("../assets/images/free-stock-image-1.jpg"),
+    "rating": 2
+  },
+  {
+    "title": "sint in architecto",
+    "description": "Quo similique iste tempore vitae. Nulla suscipit totam rem. Unde itaque tempora libero numquam dicta ullam harum error.",
+    "price": "425.00",
+    "name": "Marilyn Stoltenberg",
+    "availableDates": "2023-10-31T11:18:14.492Z",
+    "location": {
+      "city": "West Hailey",
+      "country": "Ceceliaburgh"
+    },
+    "url": require("../assets/images/free-stock-image-1.jpg"),
+    "rating": 1
+  },
+  {
+    "title": "inventore cumque quo",
+    "description": "Id doloremque tempore impedit commodi pariatur dolore commodi unde. Perferendis itaque eius cupiditate. Maiores rem minima laborum voluptatem fugiat maiores.",
+    "price": "361.00",
+    "name": "Willie Gibson",
+    "availableDates": "2023-09-10T21:28:03.935Z",
+    "location": {
+      "city": "Fort Aliza",
+      "country": "Gorczanytown"
+    },
+    "url": require("../assets/images/free-stock-image-1.jpg"),
+    "rating": 3
+  },
+  {
+    "title": "perspiciatis animi quia",
+    "description": "Culpa facilis tenetur nesciunt suscipit repellat esse velit quidem reprehenderit. Est aliquid nulla nihil ad. Tempore assumenda quam deserunt eum odit modi exercitationem.",
+    "price": "393.00",
+    "name": "Miss Yolanda Willms",
+    "availableDates": "2023-08-28T16:00:19.398Z",
+    "location": {
+      "city": "Ellaberg",
+      "country": "Fidelworth"
+    },
+    "url": require("../assets/images/free-stock-image-1.jpg"),
+    "rating": 2
+  },
+  {
+    "title": "deserunt nam omnis",
+    "description": "Iusto expedita nostrum error error aut. Ratione dolores maiores enim tempora ducimus blanditiis. Rem officiis laudantium similique blanditiis nemo necessitatibus rerum voluptatibus.",
+    "price": "385.00",
+    "name": "Inez Williamson",
+    "availableDates": "2023-11-09T07:10:39.144Z",
+    "location": {
+      "city": "Ankeny",
+      "country": "Delphinetown"
+    },
+    "url": require("../assets/images/free-stock-image-1.jpg"),
+    "rating": 5
+  },
+  {
+    "title": "quia molestias quasi",
+    "description": "Ex temporibus earum temporibus. Officia libero exercitationem neque placeat voluptate placeat animi. Corrupti hic quidem similique.",
+    "price": "398.00",
+    "name": "Janis Ward",
+    "availableDates": "2023-11-25T15:51:50.971Z",
+    "location": {
+      "city": "Cathedral City",
+      "country": "Jerdemouth"
+    },
+    "url": require("../assets/images/free-stock-image-1.jpg"),
+    "rating": 3
+  },
+  {
+    "title": "aspernatur iste occaecati",
+    "description": "Corrupti doloremque error. Maxime dicta tempora eum excepturi ducimus. Atque officiis quam deleniti.",
+    "price": "485.00",
+    "name": "Tommy Jacobs",
+    "availableDates": "2023-10-27T06:31:35.618Z",
+    "location": {
+      "city": "Nestorshire",
+      "country": "West Zoilaworth"
+    },
+    "url": require("../assets/images/free-stock-image-1.jpg"),
+    "rating": 1
+  }
+]
